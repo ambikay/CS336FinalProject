a74493b17780f74de9d96ac2d56d1d2b2cf7f7c1
Laasyasri Vaddepalli <73204164+LaasyaV@users.noreply.github.com>
2022-05-07 11:46:23 -0400

    Update BuyMeDatabaseFinal.sql

--- BuyMeDatabaseFinal.sql
+++ BuyMeDatabaseFinal.sql
@@ -11,7 +11,11 @@ create table car(item_id int, make varchar(20), model varchar(20), year varchar(
 create table motorbike(item_id int, color varchar(20), type varchar(20), company_name varchar(20), primary key (item_id), foreign key (item_id) references items(item_id));
 create table truck(item_id int, capacity int, size int, dimensions varchar(20), primary key (item_id), foreign key (item_id) references items(item_id));
 create table current_users(user_type varchar(20), id int, primary key (id), foreign key (id) references enduser(eid));
+
+-- sample data for testing
 insert into enduser(eid,login_name, email, password) values (1,"admin", "admin@gmail.com", "adminuser123");
 insert into enduser(eid,login_name, email, password) values (2,"mark", "mark@gmail.com", "markuser123");
-insert into items(item_id, current_bid) values (1,25.50);
-insert into car(item_id, make, model, year) values (1, "Honda", "Civic", "2016");
+insert into items(item_id, current_bid, start_date) values (1,25.50, curdate()), (2,20.0, curdate()), (3,10.0, curdate()), (4,30.0, curdate()), (5,29.0, curdate()), (6,18.0, curdate());
+insert into car(item_id, make, model, year) values (1, "Honda", "Civic", "2016"), (2, "Toyota", "Rav4", "2012");
+insert into motorbike(item_id, color, type, company_name) values (3, "blue", "bullet", "yamaha"), (4, "red", "speed", "royal enfield");
+insert into truck(item_id, capacity, size, dimensions) values (5, 2, 50, "2x3"), (6, 4, 80, "6x4");
